dd9dcf1736e0def86eb63af5af209add28a2270a
asib ahmed <ahmedasib1@gmail.com>
2024-09-17 18:43:04 +0600

    fix: vercel deployment

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:5173/','https://nimble-sorbet-dddb99.netlify.app/'],
+  credentials:true,
+  }
+))
 app.use(express.json())
+
+
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
@@ -23,7 +29,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("dcit");
     const newContact = database.collection("newContact");
@@ -44,8 +50,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
